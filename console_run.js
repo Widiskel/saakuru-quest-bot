e1f0f14378274386599a3774d13bc955ba810b18
widiskel <waputra16@gmail.com>
2024-05-27 19:48:43 +0700

    add console run

--- console_run.js
+++ console_run.js
@@ -91,82 +91,86 @@ const claimQuestTask = async (token, questId, taskId) => {
 };
 
 //MAIN SECTION
-console.log("========================================");
-console.log("=          Saakaru Quest Bot           =");
-console.log("=         Created by Widiskel          =");
-console.log("========================================");
-console.log();
-console.log(`Processing ACCOUNT `);
-console.log();
-
-let token = user.token;
-let refreshToken = user.refreshToken;
-
-const [newToken, newRefreshToken, userInfo] = await fetchUserInfo(
-  token,
-  refreshToken
-);
-
-if (userInfo == null) {
-  (token = newToken), (refreshToken = newRefreshToken);
-}
-console.log("========================================");
-console.log("Account information retrieved successfully");
-console.log(`ID               : ${userInfo.data.id}`);
-console.log(`Twitter ID       : ${userInfo.data.twitterId}`);
-console.log(`Twitter Handle   : ${userInfo.data.twitterHandle}`);
-console.log(`Name             : ${userInfo.data.name}`);
-
-const currentQuest = await fetchCurrentQuest(token);
-console.log("Curent Quest retrieved successfully");
-console.log(`QuestId          : ${currentQuest.data.id}`);
-console.log(`Quest Name       : ${currentQuest.data.name}`);
-console.log(`$SKR PRIZE POOL  : ${currentQuest.data.totalTokenPrize}`);
-const taskIds = currentQuest.data.tasks.map((task) => task.id);
-
-const userStatistic = await fetchUserStatistic(token, currentQuest.data.id);
-const completedTaskId = userStatistic.data.questTaskStatistic.map(
-  (task) => task.questTaskId
-);
-const uncompletedTaskIds = taskIds.filter(
-  (taskId) => !completedTaskId.includes(taskId)
-);
-
-console.log("User Quest Statistic retrieved successfully");
-let totalPoint = userStatistic.data.totalPoints;
-let multiplier = userStatistic.data.totalMultiplier;
-
-console.log(`Multiplier       : ${multiplier}x`);
-console.log(`Total Point      : ${totalPoint * multiplier} POINTS`);
-console.log(`Total Quest      : ${taskIds.length}`);
-console.log(`Completed Quest  : ${completedTaskId.length}`);
-console.log(`Uncomplete Quest : ${uncompletedTaskIds.length}`);
-
-console.log();
-console.log("========================================");
-console.log("============   DOING QUEST  ============");
-console.log("========================================");
-
-for (const taskId of uncompletedTaskIds) {
-  const taskDetails = currentQuest.data.tasks.find((tsk) => tsk.id == taskId);
-
-  // Log task details
+async function runScript() {
+  console.log("========================================");
+  console.log("=          Saakaru Quest Bot           =");
+  console.log("=         Created by Widiskel          =");
+  console.log("========================================");
   console.log();
-  console.log(`-------------------- CLAIMING -------------------`);
-  console.log(`Task ID: ${taskDetails.id}`);
-  console.log(`Task Name: ${taskDetails.name}`);
-  console.log(`Task Description: ${taskDetails.description}`);
-  console.log(`-------------------------------------------------`);
+  console.log(`Processing ACCOUNT `);
+  console.log();
 
-  try {
-    const claim = await claimQuestTask(token, currentQuest.data.id, taskId);
-    console.log(`TASK ${taskDetails.id} CLAIMED`);
-  } catch (error) {
-    // Handle error if claimQuestTask fails
-    failedLog(`Error claiming task ${taskId}`);
+  let token = user.token;
+  let refreshToken = user.refreshToken;
+
+  const [newToken, newRefreshToken, userInfo] = await fetchUserInfo(
+    token,
+    refreshToken
+  );
+
+  if (userInfo == null) {
+    (token = newToken), (refreshToken = newRefreshToken);
   }
-  console.log(`-------------------------------------------------`);
+  console.log("========================================");
+  console.log("Account information retrieved successfully");
+  console.log(`ID               : ${userInfo.data.id}`);
+  console.log(`Twitter ID       : ${userInfo.data.twitterId}`);
+  console.log(`Twitter Handle   : ${userInfo.data.twitterHandle}`);
+  console.log(`Name             : ${userInfo.data.name}`);
+
+  const currentQuest = await fetchCurrentQuest(token);
+  console.log("Curent Quest retrieved successfully");
+  console.log(`QuestId          : ${currentQuest.data.id}`);
+  console.log(`Quest Name       : ${currentQuest.data.name}`);
+  console.log(`$SKR PRIZE POOL  : ${currentQuest.data.totalTokenPrize}`);
+  const taskIds = currentQuest.data.tasks.map((task) => task.id);
+
+  const userStatistic = await fetchUserStatistic(token, currentQuest.data.id);
+  const completedTaskId = userStatistic.data.questTaskStatistic.map(
+    (task) => task.questTaskId
+  );
+  const uncompletedTaskIds = taskIds.filter(
+    (taskId) => !completedTaskId.includes(taskId)
+  );
+
+  console.log("User Quest Statistic retrieved successfully");
+  let totalPoint = userStatistic.data.totalPoints;
+  let multiplier = userStatistic.data.totalMultiplier;
+
+  console.log(`Multiplier       : ${multiplier}x`);
+  console.log(`Total Point      : ${totalPoint * multiplier} POINTS`);
+  console.log(`Total Quest      : ${taskIds.length}`);
+  console.log(`Completed Quest  : ${completedTaskId.length}`);
+  console.log(`Uncomplete Quest : ${uncompletedTaskIds.length}`);
+
+  console.log();
+  console.log("========================================");
+  console.log("============   DOING QUEST  ============");
+  console.log("========================================");
+
+  for (const taskId of uncompletedTaskIds) {
+    const taskDetails = currentQuest.data.tasks.find((tsk) => tsk.id == taskId);
+
+    // Log task details
+    console.log();
+    console.log(`-------------------- CLAIMING -------------------`);
+    console.log(`Task ID: ${taskDetails.id}`);
+    console.log(`Task Name: ${taskDetails.name}`);
+    console.log(`Task Description: ${taskDetails.description}`);
+    console.log(`-------------------------------------------------`);
+
+    try {
+      const claim = await claimQuestTask(token, currentQuest.data.id, taskId);
+      console.log(`TASK ${taskDetails.id} CLAIMED`);
+    } catch (error) {
+      // Handle error if claimQuestTask fails
+      failedLog(`Error claiming task ${taskId}`);
+    }
+    console.log(`-------------------------------------------------`);
+  }
+  console.log();
+  console.log(`ACCOUNT Process complete`);
+  console.log();
 }
-console.log();
-console.log(`ACCOUNT Process complete`);
-console.log();
+
+runScript();
